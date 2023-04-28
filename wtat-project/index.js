290181dac4faf6eda1a0000409df7495a2062492
Walter Weiss <s058099@htw-berlin.de>
2023-04-28 16:46:44 +0200

    Story_002 Accept user submitted parameters

--- wtat-project/index.js
+++ wtat-project/index.js
@@ -1,10 +1,14 @@
-
-
 const express = require("express"),
     app = express();
 
-const {showHomePage} = require("./controllers/homeController");
+const {showHomePage, savedSubmission} = require("./controllers/homeController");
 
+app.use(
+    express.urlencoded({
+        extended: false
+    })
+);
+app.use(express.json());
 
 app.set("port", process.env.PORT || 3000);
 app.get("/", (req, res) => {
@@ -19,14 +23,9 @@ app.listen(app.get("port"), () => {
 });
 
 app.get("/homepage", showHomePage);
+app.post("/homepage", savedSubmission);
 
 const layouts = require("express-ejs-layouts");
 app.set("view engine", "ejs");
 app.use(layouts);
 
-app.use(
-    express.urlencoded({
-        extended: false
-    })
-);
-app.use(express.json());
