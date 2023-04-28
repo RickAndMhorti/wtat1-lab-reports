11c08054fcc8a35f2f1316ba8c001a456bb3b91f
LeahSommer <sommer.leah@googlemail.com>
2023-04-28 13:47:43 +0200

    idex angepasst

--- wtat-project/index.js
+++ wtat-project/index.js
@@ -1,13 +1,21 @@
 
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express"),
+ app = express();
+app.set("port", process.env.PORT || 3000);
+app.get("/", (req, res) => {
+ res.send("Welcome to Confetti Cuisine!");
+});
+app.listen(app.get("port"), () => {
+ console.log(
+ `Server running at http://localhost:${app.get(
+"port"
+ )}`
+ );
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
+app.use(
+  express.urlencoded({
+  extended: false
+  })
+ );
+ app.use(express.json());
