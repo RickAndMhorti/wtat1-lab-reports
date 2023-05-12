0ed5659207ba183000bd715783b72214c9fba3a4
Walter Weiss <s058099@htw-berlin.de>
2023-05-12 16:43:01 +0200

    added Mongoose dependency

--- wtat-project/index.js
+++ wtat-project/index.js
@@ -2,6 +2,42 @@ const express = require("express"),
 app = express();
 const {showHomePage, savedSubmission, showLogIn, showElo} = require("./controllers/homeController");
 
+const mongoose= require("mongoose").default;
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/chessmateDB', {useNewUrlParser: true});
+}
+
+const MongoDB = require("mongodb").MongoClient,
+    dbURL = "mongodb://localhost:27017",
+    dbName = "chessmate";
+
+const ChessmateDB = mongoose.model("ChessmateDB", new mongoose.Schema({
+    name: String,
+    elo: Number,
+    region: String
+}));
+
+const chessmate1 = new ChessmateDB({
+   name: "Deez Nuts",
+   elo: 4444,
+   region: "NA"
+});
+chessmate1.save();
+MongoDB.connect(dbURL, (error, client) => {
+    if (error) throw error;
+    let db = client.db(dbName);
+    db.collection("chessmate")
+        .find()
+        .toArray((error, data) => {
+            if (error) throw error;
+            console.log(data);
+        });
+
+});
+
 const session = require('express-session');
 app.use(session({
   secret: '1973108824',
