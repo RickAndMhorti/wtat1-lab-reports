77bd9206732429e8d53bc6a3e6d9c2acae9d44b8
Natalia <nataliamarth@gmail.com>
2023-06-09 18:03:17 +0200

    adding CRUD functions

--- wtat-project/controllers/userController.js
+++ wtat-project/controllers/userController.js
@@ -1,16 +1,73 @@
-const getUserPage = ((req, res) => {
-    if (req.session.user) {
-        res.render('profile', { user: req.session.user });
+const User = require('../models/User');
+
+const getUserPage = (req, res) => {
+  if (req.session.user) {
+    if (req.query.edit === 'true') {
+      res.render('editUser', { user: req.session.user });
     } else {
-        res.redirect('/login');
+      res.render('profile', { user: req.session.user });
     }
-})
+  } else {
+    res.redirect('/login');
+  }
+};
+const displayAllUsers = async (req, res) => {
+    try {
+      const users = await User.find({}).exec();
+      res.render('users', { users });
+    } catch (err) {
+      console.log(err);
+      res.status(500).send('An error occurred while retrieving users.');
+    }
+  };
 
-const displayAllUsers = ((req, res) => {
+const updateUser = async (req, res) => {
+    const { id } = req.params;
+    const { username, email, password, region, eloRating } = req.body;
+  
+    try {
+        
+        const user = await User.findById(id).populate('region');
 
+        if (!user) {
+            return res.status(404).send('User not found.');
+        }
 
-})
+        user.username = username;
+        user.email = email;
+        user.password = password;
+        user.region.name = region;
+        user.eloRating = eloRating;
+
+        await user.save();
+        res.redirect('/users');
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('An error occurred while updating the user.');
+    }
+  };
+
+  const deleteUser = async (req, res) => {
+    const { id } = req.params;
+  
+    try {
+        const deletedUser = await User.findByIdAndRemove(id);
+        
+        if (!deletedUser) {
+          return res.status(404).send('User not found.');
+        }
+        
+        res.redirect('/users');
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('An error occurred while deleting the user.');
+    }
+  };
 
 module.exports = {
-    getUserPage
-}
+    getUserPage,
+    displayAllUsers,
+    updateUser,
+    deleteUser,
+};
